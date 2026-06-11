dbbe4c9e4e31f32f3823148fc6c3b41f6698dfbf
Nikola Tomic <tomicnikolaftn@gmail.com>
2026-06-11 14:18:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ProfilePictureSection from "./sections/ProfilePictureSection";
+
+export default function App() {
+  return (
+    <main className="invitation" aria-label="Wedding invitation">
+      <ProfilePictureSection />
+    </main>
+  );
+}
